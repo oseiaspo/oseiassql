d64d0fc90ae3734e53a70e2e4ccd119b6c6c456d
oseiaspo <62315798+oseiaspo@users.noreply.github.com>
2020-03-17 22:05:20 -0300

    Add files via upload

---
+++ oseias sql.sql
@@ -0,0 +1,40 @@
+# exercicio 1
+
+use pokedex; -- selecionar banco de dados 
+
+#exercicio 2
+
+desc pokemon;
+
+#atividade 3
+
+select * from pokemon ; 
+
+#atividade 4
+
+select numero, nome, cor, altura_m, peso_kg from pokemon;
+
+#atividade 5
+
+select numero, nome from pokemon
+where geracao = 1
+
+#atividade 6
+
+select nome from pokemon
+where geracao = 1
+and cor = 'amarelo'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
